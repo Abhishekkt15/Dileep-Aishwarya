59c2748861ce219a63538c24cda16a37fc1085b6
Abhishek KT <ktabhish@lentra.ai>
2024-04-15 23:56:39 +0530

    responsive code

--- js/main.js
+++ js/main.js
@@ -16,3 +16,37 @@ $(window).on("load",function(){
     }
     slideShow();
 })
+
+
+$(document).ready(function(){
+
+    // nav toggle
+    $(".hamburger-btn").click(function(){
+        $(".header .nav").slideToggle();
+    })
+    $(".header .nav a").click(function(){
+        if($(window).width() < 768){
+            $(".header .nav").slideToggle();
+        }
+    })
+    
+    //fixed header
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 100){
+            $(".header").addClass("fixed");
+        }
+        else{
+            $(".header").removeClass("fixed");
+        }
+    })
+
+    //scrollit 
+    $.scrollIt({
+        topOffset: -50
+    });
+    
+
+
+})
+
+
